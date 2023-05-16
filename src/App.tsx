8906ea67e4e2fa39170eeefdca305033b68392b2
annekarolle <102996739+annekarolle@users.noreply.github.com>
2023-05-16 10:04:39 -0300

    subindo alterações de layout e animação

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,58 @@
 import "./App.css";
 import AuthProvider from "./context/authprovider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FirstPage } from "./pages/firstPage";
 import logo from "../src/assests/img/icon.png"
 import welcome from "../src/assests/img/welcome.png"
 
-function App() {
+
+declare global {
+  interface Window {
+    particlesJS: any;
+  }
+}
+
+function App () {
   const [animationStarted, setAnimationStarted] = useState(false);
   const [buttonVisible, setButtonVisible] = useState(true);
+  const [overlayVisible, setOverlayVisible] = useState(true);
 
   const handleButtonClick = () => {
     setAnimationStarted(true);
-    setTimeout(() => {   
-      setButtonVisible(false)
-    }, 1000); 
+    setTimeout(() => {
+      setButtonVisible(false);
+    }, 1000);
   };
+
+  useEffect(() => {
+    if (animationStarted) {
+      setTimeout(() => {
+        setOverlayVisible(false);
+      }, 2000);
+    }
+  }, [animationStarted]);
+ 
 
   return (
     <AuthProvider>
       <div className="App">
-        <div className={`overlay ${animationStarted ? 'active' : ''}`}>
-          <div className="container-superior">
-            <img src={welcome} alt="" />
+        {overlayVisible && (
+          <div className={`overlay ${animationStarted ? 'active' : ''}`}>
+            <div className="container-superior">             
+              <img src={welcome} alt="" />
+            </div>
+            {buttonVisible && (
+              <button className="selo" onClick={handleButtonClick}>
+                <img src={logo} alt="" />                
+              </button>
+            )}
+            <div className="container-inferior"></div>          
           </div>
-          {buttonVisible && ( 
-            <button className="selo" onClick={handleButtonClick}>
-              <img src={logo} alt="" />
-            </button>
-          )}
-          <div className="container-inferior">
-         
-          </div>
-        </div>
+        )}
         <FirstPage />
       </div>
+     
     </AuthProvider>
-
   );
 }
 
